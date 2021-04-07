c07113f433ad7be016052bf5b5cb8592256d6460
mastermind008 <link2muzammal@gmail.com>
2021-04-07 18:21:13 +0500

    updated release request

--- src/action.js
+++ src/action.js
@@ -6,58 +6,79 @@ const { exec } = require('child_process');
 
 async function run(){
     try{
-        /*const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
         const tag_name = core.getInput('tag_name');
 
         const octokit = github.getOctokit(GITHUB_TOKEN);
 
         const { context = {} } = github;
-        const { pull_request, repository } = context.payload;*/
+        const { pull_request, repository } = context.payload;
 
-        exec('mvn -B package --file pom.xml', (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          console.log(`stdout: ${stdout}`);
-          console.error(`stderr: ${stderr}`);
-        });
-
-        /*if (tag_name != null || tag_name != '') {
+        if (tag_name != null || tag_name != '') {
 
             await octokit.repos.getReleaseByTag({
                 //Params
                 ...context.repo,
                  tag: tag_name,
 
-            }).then(function(response) {
-                //on Success
-                if (response.status == 200) {
-                    console.log("Release already exists");
+            }).then( (success) => {
+                
+                if (success.status == 200) {
+                    console.log("Release already exists.");
+                    return;
                 }
 
-            }, function(error){
-                //On failure
-                console.log("On failure");
+            }, (failure) => {
+                
+                console.log("Creating new release...");
 
-                octokit.repos.createRelease({
-                    //Params
-                    ...context.repo,
-                    tag_name: tag_name,
-                    name: tag_name,
-                    draft: false,
-                    prerelease: true
-
-                }).then(function(res){
-                    //on Success
-                    console.log("create repo response: ", res);
-
-                },function(err){
-                    //on failure
-                    console.log("create repo error: ",  err);
-                });
             });
-        }*/
+
+            exec('mvn -B package --file pom.xml', (error, stdout, stderr) => {
+
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    //exec('exit 1');
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+            });
+
+            await octokit.repos.createRelease({
+                //Params
+                ...context.repo,
+                tag_name: tag_name,
+                name: tag_name,
+                draft: false,
+                prerelease: true
+
+            }).then( (success) => {
+
+                console.log("create release success: ", success);
+
+            }, (failure) => {
+
+                console.log("create release failure: ",  failure);
+                return;
+            });
+
+            const artifactName = '';
+            exec('d target/ && ls *.jar | head -1', (error, stdout, stderr) => {
+
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    //exec('exit 1');
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+                artifactName = stdout;
+
+            });
+            //- name: GEt jar file in an env variable.
+            //run: echo "artifactName=$(cd target/ && ls *.jar | head -1)"  >> $GITHUB_ENV
+        }
 
     } catch (error) {
 
