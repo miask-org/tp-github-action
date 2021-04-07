d2aa595ec2fd09578f6f18941c617068953bbcbc
mastermind008 <link2muzammal@gmail.com>
2021-04-07 13:12:40 +0500

    updated release request

--- src/action.js
+++ src/action.js
@@ -3,30 +3,31 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run(){
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    
-    const octokit = github.getOctokit(GITHUB_TOKEN);
-
-    const { context = {} } = github;
-    const { pull_request, repository } = context.payload;
-
-    //await octokit.issues.createComment({
-    //    ... context.repo,
-    //    issue_number: pull_request.number,
-    //    body: 'Thank you submitting a pull request! We will try to review as soon as we can',
-    //  });
-
-    //console.log(pull_request.title);
-    //console.log(pull_request.body);
-    //console.log(pull_request.number);
-
-    console.log("payload: %j", context.payload)
-
     try{
-        const { status, headers, data } = await octokit.request('GET /repos/miask-org/tp-github-action/releases/tags/v2');
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+        
+        const octokit = github.getOctokit(GITHUB_TOKEN);
+
+        const { context = {} } = github;
+        const { pull_request, repository } = context.payload;
+
+        //await octokit.issues.createComment({
+        //    ... context.repo,
+        //    issue_number: pull_request.number,
+        //    body: 'Thank you submitting a pull request! We will try to review as soon as we can',
+        //  });
+
+        //console.log(pull_request.title);
+        //console.log(pull_request.body);
+        //console.log(pull_request.number);
+
+        //console.log("payload: %j", context.payload)
+
+        const { status, data } = await octokit.request('GET /repos/{repo}/releases/tags/v2', {
+            repo: repository.full_name
+        })
 
         console.log("status: ", status);
-        console.log("headers: ", headers);
         console.log("data: ", data);
 
     } catch (error) {
